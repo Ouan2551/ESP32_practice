5199ab6bc154535ce472f744f7780ddd88404cd5
🦜Nannaphat Songduong <Nannaphat2110@gmail.com>
2025-12-19 19:37:12 +0700

    -

--- learn/1.Basic/4.test_measure_voltage_INA219/4.test_measure_voltage_INA219.ino
+++ learn/1.Basic/4.test_measure_voltage_INA219/4.test_measure_voltage_INA219.ino
@@ -1,30 +1,42 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
+
 #define EXT_LED_PIN 18
 
 Adafruit_INA219 ina219;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  ina219.begin();
+  delay(1000);  // allow Serial Monitor to connect
+
+  Wire.begin(21, 22);  // ESP32 I2C pins
+
+  if (!ina219.begin()) {
+    Serial.println("INA219 not found");
+    while (1);
+  }
+
+  ina219.setCalibration_32V_2A();
+
   pinMode(EXT_LED_PIN, OUTPUT);
+
+  Serial.println("INA219 Ready");
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("____________________________");
   float voltage = ina219.getBusVoltage_V();
   float current = ina219.getCurrent_mA();
-  float power = ina219.getPower_mW();
-                    
+  float power   = ina219.getPower_mW();
+
+  Serial.println("____________________________");
   Serial.print("Voltage: ");
   Serial.print(voltage);
   Serial.println(" V");
-                    
+
   Serial.print("Current: ");
   Serial.print(current);
   Serial.println(" mA");
-                    
+
   Serial.print("Power: ");
   Serial.print(power);
   Serial.println(" mW");
